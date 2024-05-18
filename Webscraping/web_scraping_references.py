df85270cd6bdfce1bd305c8fce4ea1590bf3ff0c
Bertrand Borel <95342688+BertrandBorel@users.noreply.github.com>
2024-05-18 11:22:19 +0200

    Update web_scraping_references.py

--- Webscraping/web_scraping_references.py
+++ Webscraping/web_scraping_references.py
@@ -5,23 +5,42 @@ from tqdm.notebook import tqdm_notebook
 
 
 
+# def creation_csv(nom): # il suffit d'indiquer le nom du csv (sans l'extension)
+#     with open(nom +'.csv', 'w') as f:
+#             writer = csv.writer(f)
+#             # write the header
+#             writer.writerow(header)
+#     print("fichier créé avec le nom suivant :", nom)
 
-'''Création d'un nouveau csv : 
-Facultatif, permet l'insertion des données dans les bonnes colonnes.
-Le paramètre 'nom' permet de choisir le nom du fichier (sans l'extension .csv).
-Il n'y a pas de nom par défaut afin de ne pas écraser un fichier précédémment créé par erreur.'''
 
-def creation_csv(nom): # il suffit d'indiquer le nom du csv (sans l'extension)
+def creation_csv(nom):
+    '''Création d'un nouveau fichier CSV avec un en-tête spécifique.
+    
+    Le paramètre 'nom' permet de choisir le nom du fichier (sans l'extension .csv).
+    Il n'y a pas de nom par défaut afin de ne pas écraser un fichier précédemment créé par erreur.
+    
+    Args:
+        nom (str): Le nom du fichier CSV à créer (sans l'extension .csv).
+    '''
     header = ['Auteur(s)', 'Titre', 'Edition', 'Date', 'ISBN']
-    with open(nom +'.csv', 'w') as f:
+    
+    try:
+        with open(nom + '.csv', 'w', newline='', encoding='utf-8') as f:
             writer = csv.writer(f)
-            # write the header
             writer.writerow(header)
-    print("fichier créé avec le nom suivant :", nom)
+        print("Fichier créé avec le nom suivant :", nom + '.csv')
+    except Exception as e:
+        print(f"Une erreur est survenue lors de la création du fichier : {e}")
+
+
+
     
     
     
-'''Fonction permettant d'interroger la page d'un livre et d'en récupérer les informations. 
+
+  
+def web_scraping(nb_livres=5000, nombre=185000, nom_csv = 'ajout_fonction', site = 'https://www.leslibraires.fr/livre/'):
+    '''Fonction permettant d'interroger la page d'un livre et d'en récupérer les informations. 
 Si les informations ne sont pas trouvées sur la page, on passe alors à un autre livre.
 Fonctionne par incrémentation de chaque page web.
 Les paramètres sont :
@@ -29,8 +48,6 @@ Les paramètres sont :
   - le nombre de la page à laquelle le web scraping commence (il faut l'augmenter par défaut de 5000 à chaque passage)
   - le nom du csv où seront stockées les données
   - le site web (par défaut les libraires.fr)'''
-  
-def web_scraping(nb_livres=5000, nombre=185000, nom_csv = 'ajout_fonction', site = 'https://www.leslibraires.fr/livre/'):
     liste_totale = []
     
     for livre in tqdm_notebook(range(nb_livres)) : 
